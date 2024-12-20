85d30905409b310ae09511848960fe6d16682dab
MaxNize <maxmueller2412@gmx.de>
2024-12-20 16:02:38 +0100

    valid

--- TESTING3.py
+++ TESTING3.py
@@ -1,8 +1,4 @@
 from Programm import Programm
 
 p = Programm()
-p.UserManager.createUser("m", "m", "m", "m@m.m", "m")
-print(p.UserManager.users)
-p.UserManager.active = p.UserManager.getUser("m")
-p.UserManager.active.sendMail("test", "m@m.m", "m@m.m", "m@m.m", "m@m.m", "test", "test")
 p.mainloop()
--- db/db.py
+++ db/db.py
@@ -24,7 +24,9 @@ bcc TEXT,
 cc TEXT,
 content TEXT,
 attachmentsPath TEXT,
-folder TEXT);
+folder TEXT,
+userName Text,
+FOREIGN KEY (userName) REFERENCES users(userName));
               ''')
     c.execute('''CREATE TABLE IF NOT EXISTS contacts (
 name TEXT,
@@ -34,6 +36,157 @@ mail TEXT,
 userName Text,
 FOREIGN KEY (userName) REFERENCES users(userName));
               ''')
+
+    conn.commit()
+    conn.close()
+
+#db.setUsers([{"userName": "MMM", "firstName": "MMM","lastName": "MMM", "mail": "MMM@M.M", "pw": "123", "folders": "Inbox,Sent,Trash", "inbox": "Inbox","outbox":"Sent", "trash": "Trash"},{"userName": "M", "firstName": "M","lastName": "M", "mail": "M@M.M", "pw": "123", "folders": "Inbox,Sent,Trash", "inbox": "Inbox","outbox":"Sent", "trash": "Trash"}])       
+def getUsers():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM users')
+    users = c.fetchall()
+
+    conn.close()
+    return users
+
+def setUsers(data):
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    # Prepare the SQL INSERT statement
+    sql = '''
+    INSERT INTO users (userName, firstName, lastName, mail, pw, folders, inbox, outbox, trash)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    '''
+
+    # Prepare data as a list of tuples
+    values = [
+        (
+            mail["userName"],
+            mail["firstName"],
+            mail["lastName"],
+            mail["mail"],
+            mail["pw"],
+            mail["folders"],
+            mail["inbox"],
+            mail["outbox"],
+            mail["trash"]
+        )
+        for mail in data
+    ]
+
+    # Use executemany to insert all records
+    c.executemany(sql, values)
+
+    conn.commit()
+    conn.close()
+
+def deleteUsers():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    c.execute('DELETE FROM users')
+
+    conn.commit()
+    conn.close()
+
+#db.setMails([{"topic": "test", "to": "m@m.m", "sender": "m@m.m", "bcc": "m@m.m", "cc": "m@m.m", "content": "test", "attachmentsPath": "test", "folder": "Sent", "userName": "m"}])
+def getMails():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM mails')
+    mails = c.fetchall()
+
+    conn.close()
+    return mails
+
+def setMails(data):
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    # Prepare the SQL INSERT statement
+    sql = '''
+    INSERT INTO mails (topic, "to", sender, bcc, cc, content, attachmentsPath, folder, userName)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    '''
+
+    # Prepare data as a list of tuples
+    values = [
+        (
+            mail["topic"],
+            mail["to"],
+            mail["sender"],
+            mail["bcc"],
+            mail["cc"],
+            mail["content"],
+            mail["attachmentsPath"],
+            mail["folder"],
+            mail["userName"]
+        )
+        for mail in data
+    ]
+
+    # Use executemany to insert all records
+    c.executemany(sql, values)
+
+    conn.commit()
+    conn.close()
+
+def deleteMails():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    c.execute('DELETE FROM mails')
+
+    conn.commit()
+    conn.close()
+
+def getContacts():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM contacts')
+    contacts = c.fetchall()
+
+    conn.close()
+    return contacts
+
+def setContacts(data):
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    # Prepare the SQL INSERT statement
+    sql = '''
+    INSERT INTO contacts (name, firstName, lastName, mail, userName)
+    VALUES (?, ?, ?, ?, ?)
+    '''
+
+    # Prepare data as a list of tuples
+    values = [
+        (
+            contact["name"],
+            contact["firstName"],
+            contact["lastName"],
+            contact["mail"],
+            contact["userName"]
+        )
+        for contact in data
+    ]
+
+    # Use executemany to insert all records
+    c.executemany(sql, values)
+
+    conn.commit()
+    conn.close()
+
+def deleteContacts():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    c.execute('DELETE FROM contacts')
 
     conn.commit()
     conn.close()
